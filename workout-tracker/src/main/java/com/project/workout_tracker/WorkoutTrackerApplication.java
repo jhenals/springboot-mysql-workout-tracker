f93a0e0463608e345a7c7486c3901318602322d3
Jhenals <suboljhenalyn3@gmail.com>
2025-04-10 12:42:22 +0200

    update files

--- workout-tracker/src/main/java/com/project/workout_tracker/WorkoutTrackerApplication.java
+++ workout-tracker/src/main/java/com/project/workout_tracker/WorkoutTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.example.workout_tracker;
+package com.project.workout_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- workout-tracker/src/test/java/com/project/workout_tracker/WorkoutTrackerApplicationTests.java
+++ workout-tracker/src/test/java/com/project/workout_tracker/WorkoutTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.workout_tracker;
+package com.project.workout_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
